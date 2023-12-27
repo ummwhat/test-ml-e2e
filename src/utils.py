2d31c670425f74cb3eb20c73559c6f956fb943e5
ummwhat <adi23arora@gmail.com>
2023-12-27 20:52:22 +0530

    model running successfully

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path,obj):
     try:
@@ -19,13 +21,18 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(xtrain,ytrain,xtest,ytest,models):
+def evaluate_model(xtrain,ytrain,xtest,ytest,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(xtrain,ytrain)
+
+            model.set_params(**gs.best_params_)
             model.fit(xtrain,ytrain)
 
             ytrain_pred = model.predict(xtrain)
@@ -42,3 +49,12 @@ def evaluate_model(xtrain,ytrain,xtest,ytest,models):
     
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def load_model(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pkl.load(file_obj)
+    
+    except Exception as e:
+        return CustomException(e,sys)
